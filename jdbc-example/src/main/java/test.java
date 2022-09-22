0b3730ff02708d1947ac78ff4fb2a8cbb18df995
Simon Shanks <sshanks@kx.com>
2022-09-22 16:14:49 +0100

    Better error handling/logging - fixed some sonar

--- jdbc-example/src/main/java/test.java
+++ jdbc-example/src/main/java/test.java
@@ -1,16 +1,23 @@
 // q -p 5001   use q/c/jdbc.jar
 import java.sql.*;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class test{
-  public static void main(String args[]){
+  private static final Logger LOGGER = Logger.getLogger(test.class.getName());
+  public static void main(String[] args){
+    System.setProperty("java.util.logging.SimpleFormatter.format","[%1$tF %1$tT] [%4$-7s] %5$s %n");
+    Connection h = null;
+    Statement st = null;
+    PreparedStatement p = null;
     try{
       Class.forName("jdbc");  //loads the driver 
-      Connection h = DriverManager.getConnection("jdbc:q:localhost:5001","","");
-      Statement e = h.createStatement();
-      e.executeUpdate("create table t(x int,f float,s varchar(0),d date,t time,z timestamp)");
+      h = DriverManager.getConnection("jdbc:q:localhost:5001","","");
+      st = h.createStatement();
+      st.executeUpdate("create table t(x int,f float,s varchar(0),d date,t time,z timestamp)");
       //e.execute("insert into t values(9,2.3,'aaa',date'2000-01-02',time'12:34:56',timestamp'2000-01-02 12:34:56)");
 
-      PreparedStatement p = h.prepareStatement("q){`t insert 0N!a::x}"); //insert into t values(?,?,?,?,?,?)");
+      p = h.prepareStatement("q){`t insert 0N!a::x}"); //insert into t values(?,?,?,?,?,?)");
       p.setInt(1,2);
       p.setDouble(2,2.3);p.setString(3,"qwe");
       p.setDate(4,Date.valueOf("2000-01-02"));
@@ -18,16 +25,19 @@ public class test{
       p.setTimestamp(6,Timestamp.valueOf("2000-01-02 12:34:56"));
       p.executeUpdate();
 
-      ResultSet r = e.executeQuery("select * from t");
+      ResultSet r = st.executeQuery("select * from t");
       ResultSetMetaData m=r.getMetaData();
       int n=m.getColumnCount();
-      for(int i=0;i<n;)System.out.println(m.getColumnName(++i));
-      while(r.next())for(int i=0;i<n;)System.out.println(r.getObject(++i));
-
-      h.close();
+      for(int i=1;i<=n;++i)LOGGER.log(Level.INFO,m.getColumnName(i));
+      while(r.next())for(int i=1;i<=n;++i)LOGGER.log(Level.INFO,r.getObject(i).toString());
     }
     catch(Exception e){
-      System.out.println(e.getMessage());
+      LOGGER.log(Level.SEVERE,e.getMessage());
+    }
+    finally{
+      try{p.close();}catch(Exception e){/*ignored*/}
+      try{st.close();}catch(Exception e){/*ignored*/}
+      try{h.close();}catch(Exception e){/*ignored*/}
     }
   } 
 }
